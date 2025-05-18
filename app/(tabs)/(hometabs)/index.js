97c7736e06d38e7e4d9c634bc65af06a68b09248
Rokyou <rokyou360@gamil.com>
2025-05-19 01:40:03 +0530

    df

--- app/(tabs)/(hometabs)/index.js
+++ app/(tabs)/(hometabs)/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import FouroneCard from '../../components/cards/FouroneCard';
+import FouroneCard from '../../../components/cards/FouroneCard';
 
 const data = [1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; // Dummy data
 
